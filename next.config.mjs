1bbdbebeb1568d48bdfafb34d7016e8ae7e88e21
ZP LAO <LaozpGZ@gmail.com>
2025-04-05 12:27:41 +0800

    0.1.3

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,9 @@ const nextConfig = {
   output: "export",
   reactStrictMode: true,
   transpilePackages: ["wallet-adapter-react", "wallet-adapter-plugin"],
-  assetPrefix: isProd ? "/aptos-wallet-adapter/nextjs-cross-chain-example" : "",
-  basePath: isProd ? "/aptos-wallet-adapter/nextjs-cross-chain-example" : "",
+  // 在Vercel上部署时，不需要这些路径前缀
+  // assetPrefix: isProd ? "/aptos-wallet-adapter/nextjs-cross-chain-example" : "",
+  // basePath: isProd ? "/aptos-wallet-adapter/nextjs-cross-chain-example" : "",
   webpack: (config) => {
     config.resolve.fallback = { "@solana/web3.js": false };
     return config;
